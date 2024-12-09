c7bb9d0cbf3406bdacf7fe762c4912733147683a
HAKSUYOON <hakssu93@gmail.com>
2024-12-10 08:30:33 +0900

    feat : 회원 가입, 회원 로그인, 회원 상세정보 기능 추가 refactor : SecurityConfig 파일 API 에 맞게 수정

--- src/main/java/com/zerobase/cafekiosk/config/SecurityConfig.java
+++ src/main/java/com/zerobase/cafekiosk/config/SecurityConfig.java
@@ -1,20 +1,45 @@
 package com.zerobase.cafekiosk.config;
 
+import com.zerobase.cafekiosk.member.Service.MemberServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final JwtAuthenticationFilter authenticationFilter;
+  private final MemberServiceImpl memberServiceImpl;
+  private final PasswordEncoder passwordEncoder;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.httpBasic().disable();
+
     http.csrf().disable();
 
-    http.authorizeRequests().antMatchers("/").permitAll();
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-    super.configure(http);
+    http.authorizeRequests().antMatchers("/main/**", "/member/signin", "/member/signup")
+        .permitAll();
+
+    http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+
+    http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(memberServiceImpl).passwordEncoder(passwordEncoder);
   }
 }
