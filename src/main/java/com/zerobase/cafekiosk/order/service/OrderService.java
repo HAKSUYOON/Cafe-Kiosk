fb69075f9739896fc8633a2e8ec4909593080ad5
HAKSUYOON <hakssu93@gmail.com>
2024-12-12 00:03:41 +0900

    refactor : 사용하지 않는 import 문 삭제

--- src/main/java/com/zerobase/cafekiosk/order/service/OrderService.java
+++ src/main/java/com/zerobase/cafekiosk/order/service/OrderService.java
@@ -1,7 +1,6 @@
 package com.zerobase.cafekiosk.order.service;
 
 import com.zerobase.cafekiosk.order.dto.OrderDto;
-import com.zerobase.cafekiosk.order.model.OrderInput;
 
 public interface OrderService {
 
