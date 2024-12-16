d0e5b0eb869ebac2bb082f46b7a8d84b5962d560
HAKSUYOON <hakssu93@gmail.com>
2024-12-16 21:36:09 +0900

    refactor : Exception 수정

--- src/main/java/com/zerobase/cafekiosk/exception/Impl/NotFoundCardException.java
+++ src/main/java/com/zerobase/cafekiosk/exception/Impl/NotFoundCardException.java
@@ -12,6 +12,10 @@ public class NotFoundCardException extends AbstractException {
 
   @Override
   public String getMessage() {
-    return "인증되지 않은 카드입니다.";
+    return "인증되지 않은 카드입니다. 사용할 수 있는 카드는 아래와 같습니다.\n"
+        + "1111_1111_1111_1111 : 체크카드, 사용가능\n"
+        + "2222_2222_2222_2222 : 신용카드, 사용가능\n"
+        + "3333_3333_3333_3333 : 체크카드, 거래정지\n"
+        + "4444_4444_4444_4444 : 신용카드, 거래정지";
   }
 }
